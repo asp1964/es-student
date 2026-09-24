5ea0fd033805346d7f311f445f57f8c38f130322
asp1964 <a5proninas@yandex.com>
2026-09-24 19:32:32 +0700

    Сделана п1.3.3

---
+++ 133-led-button-usb/main.c
@@ -0,0 +1,58 @@
+
+#include "pico/stdlib.h"
+// добавляем заголовочный файл функций ввода-вывода
+#include <stdio.h>
+#include "hardware/gpio.h"
+// добавляем заголовочный файл функций работы с GPIO
+
+const uint DEBOUNCE_MS = 20;
+const uint BUTTON_PIN = 15;
+const uint LED_PIN = 18;
+// объявляем константу вывода светодиода
+
+bool get_button_debounce(uint pin)
+{
+    bool state = gpio_get(pin);
+    sleep_ms(DEBOUNCE_MS);
+    return state && gpio_get(pin);
+}
+
+void set_led(bool on)
+{
+    gpio_put(LED_PIN, on);
+    printf("led %s\n", on ? "on" : "off");
+}
+
+int main()
+{
+    stdio_init_all();
+     
+    gpio_init(LED_PIN);
+     
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+   
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
+
+    bool led = false;
+    bool previous = false;
+
+    while (1)
+    {
+       
+       bool current = get_button_debounce(BUTTON_PIN);
+       
+
+        if (previous == true && current == false)
+        {
+            led = !led;
+            set_led(led);
+            //gpio_put(LED_PIN, led);
+        }
+        
+
+        previous = current;
+        sleep_ms(10);
+    }
+}
